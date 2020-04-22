db7fd72e498a9f63664c8d5a1e3fab46b899d53a
Derek phung <dragevards@gmail.com>
2020-04-22 20:38:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,14 +11,23 @@ class String
   end
 
   def question?
-
+    if self.end_with("?")
+      true
+    else 
+      false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else 
+      false 
+    end
   end
 
   def count_sentences
-
+    array = self.split(".", "?", "!")
+    array.count 
   end
 end
