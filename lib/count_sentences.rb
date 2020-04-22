5c28b7cb8c830a109e0506e899153e584440e724
Derek phung <dragevards@gmail.com>
2020-04-22 20:41:02 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,8 +29,9 @@ class String
   def count_sentences
     array = []
     array = self.split(".", "?", "!")
-    array.delete_if |x|
-      x == []
+    array.delete_if do |x|
+      x.empty
+    end 
     array.count 
   end
 end
